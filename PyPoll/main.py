f0b9bf6b1766d416eff5bbce6430a86b15cd770f
Ying <m.villacreses18@gmail.com>
2018-11-27 21:29:02 -0500

    Final Results for Pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 poll_infile = os.path.join(".", "election_data.csv")
-#poll_outfile = os.path.join(".", "election_results.txt")
+poll_outfile = os.path.join(".", "election_results.txt")
 
 #List to store data
 CandidatesList = []
@@ -10,11 +10,15 @@ KhanV = []
 Correy = []
 Li = []
 otherc = []
+KhanV2 = 0
+Correy2 = 0
+Li2 = 0
+Other2 = 0
 KhanPercent = 0
 CorreyPercent = 0
 LiPercent = 0
 OTooleyPercent = 0
-WinnerCounter = {}
+WinnerCounter = []
 
 #find total # months and total net value, loop
 with open(poll_infile, 'r') as csvfile:
@@ -33,24 +37,45 @@ with open(poll_infile, 'r') as another:
     for eachword in CandidatesList:
         if eachword == "Khan":
             KhanV.append("1")
+            KhanV2 += 1
         elif eachword == "Correy":
             Correy.append("1")
+            Correy2 += 1
         elif eachword == "Li":
             Li.append("1")
+            Li2 += 1
         else:
             otherc.append("1")
+            Other2 += 1
 
 counterKha = sum(1 for numb in KhanV)
-#WinnerCounter.append(counterKha, "Khan")
+WinnerCounter.append(counterKha)
 counterCor = sum(1 for numb in Correy)
-#WinnerCounter.append(counterCor)
+WinnerCounter.append(counterCor)
 counterLi = sum(1 for numb in Li)
-#WinnerCounter.append(counterLi)
+WinnerCounter.append(counterLi)
 counterOte = sum(1 for numb in otherc)
-#WinnerCounter.append(counterOte)
+WinnerCounter.append(counterOte)
 
-#winnerYOU = max(WinnerCounter)
-#print(winnerYOU)
+Consolidated_count = {"Khan": KhanV2,
+                     "Correy": Correy2,
+                    "Li": Li2,
+                    "Other": Other2}
+
+for winner in Consolidated_count:
+    if KhanV2 > Correy2:
+        winnerCand = "Khan"
+        break
+        if Correy2 > Li2:
+            winnerCand = "Correy"
+            break
+            if Li2 > Other2:
+                winnerCand = "Li"
+                break
+                if Other2 > KhanV2:
+                    winnerCand = "O'Tooley"
+
+W = ("Winner: " + str(winnerCand))
 
 KhanPercent =("Khan: {0:.0%}".format(counterKha/vote_count))
 CorreyPercent =("Correy: {0:.0%}".format(counterCor/vote_count))
@@ -64,7 +89,7 @@ C = (str(CorreyPercent) + " " +"(" + str(counterCor) + ")")
 L = (str(LiPercent) + " " +"(" + str(counterLi) + ")")
 O = (str(OTooleyPercent) + " " +"(" + str(counterOte) + ")")
 #! Sill working on extracting "khan" as the winner
-Winner = "Winner: Khan"
+#Winner = "Winner: Khan"
 #winner =max()
 print(f"{header}\n"
       f"{skipper}\n"
@@ -75,9 +100,17 @@ print(f"{header}\n"
       f"{L}\n"
       f"{O}\n"
       f"{skipper}\n"
-      f"{Winner}\n"
+      f"{W}\n"
       f"{skipper}\n")
 
+finalR = [header, skipper, tol_count, skipper,
+          K, C, L, O, skipper, W, skipper]
+
+with open(poll_outfile, "w") as resultspoll:
+    writpoll = csv.writer(resultspoll)
+
+    for row in finalR:
+        resultspoll.write(row + '\n')
 
 
 
